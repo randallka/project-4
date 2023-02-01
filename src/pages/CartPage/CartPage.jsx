b9cab44486000393080715935bc03aed25e58f05
randallka-ga <randallkaem@gmail.com>
2023-01-31 18:56:05 -0700

    psuedo code for tomorrow

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useContext, useState } from "react";
+import UserContext from "../../App";
 function Cartpage() {
-    return ( <div>Customer cart</div> );
+  const user = useContext(UserContext);
+  const [cart, setcart] = useState([]);
+  const [cartinfo, setCartInfo] = useState({
+    total: "", 
+    itemCount: "",
+  })
+  useEffect(() => {
+    async function getCart() {
+      try {
+        //const cart = await cartApi.getcart(user._id)
+        //setCart(cart.items)
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getCart()
+    // const cartTotal = cart.map(item => item?.price).reduce((prev, next) => prev + next)
+    // setcart({ 
+    //     ...cart,
+    //     total: cartTotal,
+    // })
+  }, []);
+
+  async function removeItemFromCart(id) {
+    try {
+      //await cartApi.removeItem
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return <div>Customer cart</div>;
+  //show items with a delete button 
 }
 
 export default Cartpage;
