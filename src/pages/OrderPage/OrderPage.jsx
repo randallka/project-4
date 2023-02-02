3e3be8f2ef01560473fb22d09fd53d8d35e35794
randallka-ga <randallkaem@gmail.com>
2023-02-01 19:50:28 -0700

    psuedo code

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,5 +1,20 @@
+import { useParams } from "react-router";
+import { useContext } from "react";
+import { UserContext } from "../../App";
 function OrderPage() {
-    return ( <div>Order Page</div> );
+const user = useContext(UserContext)
+    //get customer orders
+
+    //get Restaurant Orders
+
+    return  (
+        user?.isRestaurantOwner ? 
+            <div>restaurant order page</div>
+            : 
+            <div>customer order page</div>
+    )
+         
+     
 }
 
 export default OrderPage;
